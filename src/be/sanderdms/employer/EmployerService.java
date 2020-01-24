183b2858026e583cf938900ea14767889cd2fde5
Sander <sander@asocialmedia.be>
2020-01-24 14:37:11 +0100

    add javadocs

--- src/be/sanderdms/employer/EmployerService.java
+++ src/be/sanderdms/employer/EmployerService.java
@@ -6,31 +6,54 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class EmployerService {
-	@Autowired
-	private EmployerRepository repo;
-	
-	public List<Employer> listAll(){
-		return (List<Employer>) repo.findAll();
-	}
-	
-	public void save(Employer employer) {
-		repo.save(employer);
-	}
-	
-	public Employer getById(long id) {
-		Optional<Employer> result = repo.findById(id);
-		return result.get();
-		
-	}
-	
-	public void delete(long id) {
-    	repo.deleteById(id);
+    @Autowired
+    private EmployerRepository repo;
+
+    /**
+     * Lists all rows in Employer table
+     * 
+     * @return A list with Employer objects. One object for each row in database.
+     */
+    public List<Employer> listAll() {
+        return (List<Employer>) repo.findAll();
     }
-	
-	public Long countItems() {
-		return repo.count();
-	}
+
+    /**
+     * Save an employer object to database
+     * 
+     * @param employer The Employer object
+     */
+    public void save(Employer employer) {
+        repo.save(employer);
+    }
+
+    /**
+     * 
+     * @param id
+     * @return the Employer object matching the requested id or empty if no result found
+     */
+    public Employer getById(long id) {
+        Optional<Employer> result = repo.findById(id);
+        return result.get();
+
+    }
+
+    /**
+     * 
+     * @param id the id to delete from database
+     */
+    public void delete(long id) {
+        repo.deleteById(id);
+    }
+
+    /**
+     * Count the items present in database
+     * 
+     * @return a Long representing the amount of items in database
+     */
+    public Long countItems() {
+        return repo.count();
+    }
 }
