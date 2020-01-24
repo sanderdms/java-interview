c9a2c47ece6dc8589cdda7f2f0b10dbb775a03f0
sanderdms <sanderdooms@live.be>
2020-01-24 10:38:48 +0100

    refactor indentations

--- src/be/sanderdms/employer/EmployerController.java
+++ src/be/sanderdms/employer/EmployerController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -22,33 +23,26 @@ public class EmployerController {
 	@Autowired
 	private EmployerService service;
 
-
     @RequestMapping("/")
     public ModelAndView home(){
         ModelAndView mav = new ModelAndView("index");
-        
         List<Employer> listEmployer =  service.listAll();
-        
         mav.addObject("listcount", service.countItems());
         mav.addObject("list", listEmployer);
         return mav;
     }
     
-    
     @RequestMapping("/new")
     public String newEmployerForm(final Model model) {
-    	   if(!model.containsAttribute("employer")){
-    	        model.addAttribute("employer", new Employer());
-    	    }
+	   if(!model.containsAttribute("employer")){
+	        model.addAttribute("employer", new Employer());
+	    }
     	return "new_employer";
-    	//https://stackoverflow.com/questions/15903238/passing-bindingresult-through-redirectionattributes
     }
-    
     
     @RequestMapping("/edit")
     public ModelAndView editEmployerForm(@RequestParam long id, final Model model){
     	ModelAndView mav = new ModelAndView("edit_employer");
-    	
     	// Model can already be provided in case of errors during /save method
     	// Check if model is already set, else take values from database
     	if(!model.containsAttribute("employer")) {
@@ -66,21 +60,17 @@ public class EmployerController {
     	return "redirect:/";
     }
     
-   
-   
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String saveEmployer(@Valid @ModelAttribute("employer") Employer employer, BindingResult binding, final HttpServletRequest request, RedirectAttributes attr ) {
 		if(binding.hasErrors()) {
-        attr.addFlashAttribute("org.springframework.validation.BindingResult.employer", binding);
-    	attr.addFlashAttribute("employer", employer); //--> https://github.com/spring-projects/spring-framework/issues/23589
-		String referer = request.getHeader("referer");
-    	return "redirect:" +referer;
+	        attr.addFlashAttribute("org.springframework.validation.BindingResult.employer", binding);
+	    	attr.addFlashAttribute("employer", employer); // Make sure WebMvcConfigurer is annotated with @EnableWebMvc
+			String referer = request.getHeader("referer");
+	    	return "redirect:" +referer;
     	}else {
         	service.save(employer);
     	}
-    	
     	return "redirect:/";
     }
-    
     
 }
