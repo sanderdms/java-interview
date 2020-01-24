13762373ca806e458990669b3c26ac81f7fe8bf3
Sander <sander@asocialmedia.be>
2020-01-24 14:14:16 +0100

    add javadoc comments

--- src/be/sanderdms/utils/MyUtils.java
+++ src/be/sanderdms/utils/MyUtils.java
@@ -3,15 +3,15 @@ package be.sanderdms.utils;
 import java.nio.charset.StandardCharsets;
 
 public class MyUtils {
-	
-	/**
-	 * A helper method to capitalize strings. (First character is uppercase)
-	 * 
-	 * @param str The String that needs to be capitalized
-	 * @return The String output
-	 * */
+
+    /**
+     * A helper method to capitalize strings. (First character is uppercase)
+     * 
+     * @param str The String that needs to be capitalized
+     * @return The String output
+     */
     public static String capitalize(String str) {
-        if(str == null || str.isEmpty()) {
+        if (str == null || str.isEmpty()) {
             return str;
         }
         return str.substring(0, 1).toUpperCase() + str.substring(1);
@@ -23,7 +23,7 @@ public class MyUtils {
      * @param str The String that needs to be converted to UTF-8
      * @return The String UTF-8 encoded
      */
-    public static String encodeUTF8String(String str){
+    public static String encodeUTF8String(String str) {
         byte[] bytes = str.getBytes(StandardCharsets.ISO_8859_1);
         str = new String(bytes, StandardCharsets.UTF_8);
         return str;
